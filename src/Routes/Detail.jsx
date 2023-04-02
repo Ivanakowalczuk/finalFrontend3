2d5ea67d4b3599121b83a66090f910c14ee6b057
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2023-04-02 20:44:57 -0300

    agregando favoritos

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
   const [dentist, setDentist] = useState([])
-
+ 
+  const navigate = useNavigate()
   const params = useParams()
 
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
@@ -17,6 +18,10 @@ const Detail = () => {
        .then(res => res.json())
        .then(data => setDentist(data))
   },[])
+
+  const addFav= () => {
+    localStorage.setItem('dentist', JSON.stringify(dentist) )
+  }
   return (
     <>
       <h1>Detail Dentist {params.id} </h1>
@@ -26,7 +31,9 @@ const Detail = () => {
         <h4>{dentist.email}</h4>
         <p>{dentist.phone}</p>
         <p>{dentist.website}</p>
+        <button onClick={addFav}>AddFav</button>
         </div>
+        <button onClick={() => navigate(-1)}>Go back</button>
      
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
